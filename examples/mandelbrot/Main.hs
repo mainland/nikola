8f94f5546c13bd2410bb42faed6918480452f36b
Geoffrey Mainland <mainland@apeiron.net>
2012-10-08 12:23:55 +0100

    Add benchmark mode for mandelbrot example.

--- examples/mandelbrot/Main.hs
+++ examples/mandelbrot/Main.hs
@@ -1,11 +1,16 @@
 module Main where
 
-import Data.Array.Repa
+import Control.DeepSeq
+import qualified Criterion.Main as C
+import Data.Array.Repa hiding ((++))
 import Data.Array.Repa.Eval
 import Data.Array.Repa.Repr.ForeignPtr
 import Data.Word
 import Foreign (castForeignPtr)
-import System.Environment (getArgs)
+import qualified Graphics.Rendering.OpenGL as GL
+import System.Environment (getArgs, withArgs)
+
+import qualified Data.Array.Nikola.Backend.CUDA as N
 
 import qualified Mandelbrot.NikolaV1 as MN1
 import qualified Mandelbrot.NikolaV2 as MN2
@@ -26,6 +31,12 @@ type Bitmap r = Array r DIM2 RGBA
 
 type MBitmap r = MVec r RGBA
 
+instance (Shape sh, Source r e) => NFData (Array r sh e) where
+    rnf arr = deepSeqArray arr ()
+
+instance NFData GL.BufferObject where
+    rnf pbo = seq pbo ()
+
 defaultView :: G.View
 defaultView = G.View { G.left  = -0.25
                      , G.bot   = -1.0
@@ -35,10 +46,66 @@ defaultView = G.View { G.left  = -0.25
 
 main :: IO ()
 main = do
-    (opts, _) <- getArgs >>= parseArgs defaultConfig
-    let size    = fromIntegral $ fromLJust confSize opts
-        limit   = fromIntegral $ fromLJust confLimit opts
-        backend = fromLJust confBackend opts
+    args <- getArgs
+    case args of
+      "--benchmark":args' -> doBenchmark args'
+      _                   -> doMandelbrot
+
+doBenchmark :: [String] -> IO ()
+doBenchmark args =
+    withArgs args $ do
+    G.initializeGLUT False
+    G.openWindowGLUT disp
+    N.initializeCUDAGLCtx N.DeviceListAll
+    repav2Gen   <- MR2.mandelbrotImageGenerator
+    nikolav2Gen <- MN2.mandelbrotImageGenerator
+    nikolav3Gen <- MN3.mandelbrotImageGenerator
+    nikolav4Gen <- MN4.mandelbrotImageGenerator
+    let generateBitmapFrame :: Backend -> G.View -> Int -> Int -> IO (Bitmap F)
+        generateBitmapFrame RepaV1 (G.View lowx lowy highx highy) size limit =
+            MR1.mandelbrotImage lowx lowy highx highy size size limit
+
+        generateBitmapFrame RepaV2 (G.View lowx lowy highx highy) size limit =
+            repav2Gen lowx lowy highx highy size size limit
+
+        generateBitmapFrame NikolaV1 (G.View lowx lowy highx highy) size limit =
+            MN1.mandelbrotImage lowx lowy highx highy size size limit
+
+        generateBitmapFrame NikolaV2 (G.View lowx lowy highx highy) size limit =
+            nikolav2Gen lowx lowy highx highy size size limit
+
+        generateBitmapFrame NikolaV3 (G.View lowx lowy highx highy) size limit =
+            nikolav3Gen lowx lowy highx highy size size limit
+
+        generateBitmapFrame backend _ _ _ =
+            fail $ "Cannot generate bitmap for" ++ show backend
+
+    let generatePBOFrame :: Backend -> G.View -> Int -> Int -> IO GL.BufferObject
+        generatePBOFrame NikolaV4 (G.View lowx lowy highx highy) size limit =
+            nikolav4Gen lowx lowy highx highy size size limit
+
+        generatePBOFrame backend _ _ _ =
+            fail $ "Cannot generate PBO for" ++ show backend
+
+    C.defaultMain
+         [C.bench "Repa V1" $ C.nfIO (generateBitmapFrame RepaV1 defaultView size limit)
+         ,C.bench "Repa V2" $ C.nfIO (generateBitmapFrame RepaV2 defaultView size limit)
+         ,C.bench "Nikola V1" $ C.nfIO (generateBitmapFrame NikolaV1 defaultView size limit)
+         ,C.bench "Nikola V2" $ C.nfIO (generateBitmapFrame NikolaV2 defaultView size limit)
+         ,C.bench "Nikola V3" $ C.nfIO (generateBitmapFrame NikolaV3 defaultView size limit)
+         ,C.bench "Nikola V4" $ C.nfIO (generatePBOFrame NikolaV4 defaultView size limit)
+         ]
+  where
+    size  = 512
+    limit = 255
+    disp  = G.InWindow "Mandelbrot" (size, size) (10, 10)
+
+doMandelbrot :: IO ()
+doMandelbrot = do
+    (config, _) <- getArgs >>= parseArgs defaultConfig
+    let size    = fromIntegral $ fromLJust confSize config
+        limit   = fromIntegral $ fromLJust confLimit config
+        backend = fromLJust confBackend config
         disp    = G.InWindow "Mandelbrot" (size, size) (10, 10)
     G.display disp defaultView (frameGenerator backend limit)
 
